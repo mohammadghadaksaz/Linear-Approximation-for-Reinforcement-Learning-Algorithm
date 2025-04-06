b4fd48ac6a8750d758c479078c33bafca9c15d84
Mohammad Ghadaksaz <mohammad_ghadaksaz@yahoo.com>
2025-04-06 16:04:12 -0400

    REINFORCE_Agent is compeleted

--- src/Agents/REINFORCE_Agent.py
+++ src/Agents/REINFORCE_Agent.py
@@ -16,3 +16,22 @@ class REINFORCE:
         self.theta = np.random.uniform(-0.0001, 0.0001,
          (self.env.action_space.n,  num_tiles * num_bins 
                         * env.observation_space.shape[0]))
+        
+    def softmax(self, state):
+        preferences = np.exp(np.sum((self.theta * state) / self.temp, axis=1))
+        return preferences / np.sum(preferences)
+    
+    def choose_action(self, state):
+        action_probs = self.softmax(state)
+        actions = np.array(range(self.env.action_space.n))
+        return np.random.choice(actions, p=action_probs)
+    
+    def update(self, state, G, action):
+        Qs = np.sum(np.exp(np.sum((self.theta * state) / self.temp, axis=1)))
+        self.theta[action] += self.alpha * G * \
+            (state / self.temp - state / (Qs * self.temp) \
+             * np.exp(np.sum(self.theta[action] * state) / self.temp) )
+
+    def temp_decay(self):
+        if self.temp > self.temp_stop:
+            self.temp *= self.temp_dec
