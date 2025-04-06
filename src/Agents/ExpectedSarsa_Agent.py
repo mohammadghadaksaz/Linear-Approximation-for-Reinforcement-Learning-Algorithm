026dc3ae30c9ee973470dfc667d20098db7b1004
Mohammad Ghadaksaz <mohammad_ghadaksaz@yahoo.com>
2025-04-06 15:58:25 -0400

    ExpectedSarsa_Agent is compeleted

--- src/Agents/ExpectedSarsa_Agent.py
+++ src/Agents/ExpectedSarsa_Agent.py
@@ -13,3 +13,23 @@ class ExpectedSarsa:
         self.num_bins = num_bins
         self.theta = np.random.uniform(-0.001, 0.001, (self.env.action_space.n,
                         num_tiles * num_bins * env.observation_space.shape[0]))
+        
+    def choose_action(self, state):
+        if (random.random() < self.epsilon):
+            action = random.randint(0, self.env.action_space.n - 1)
+        else:
+            action = np.argmax(np.sum(self.theta * state, axis=1))
+        return action
+
+    def update(self, state, action, reward, next_state, done):
+        if done:
+            target = reward
+        else:
+            Q = np.sum(self.theta * state, axis=1)
+            probabilities = [self.epsilon if i == np.argmax(Q) else
+                   (1 - self.epsilon)/(self.env.action_space.n - 1)
+                      for i in range(self.env.action_space.n)]
+            target = reward + self.gamma * np.sum(probabilities * 
+                        (np.sum(self.theta * next_state, axis=1)))
+        self.theta[action] += self.alpha * (target - 
+                    np.sum(self.theta[action] * state)) * state
