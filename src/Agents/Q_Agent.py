93ba2b9705b2fd2eb0cf56fecd863efc04d148e5
Mohammad Ghadaksaz <mohammad_ghadaksaz@yahoo.com>
2025-04-06 15:13:17 -0400

    Q_Agent class is completed

--- src/Agents/Q_Agent.py
+++ src/Agents/Q_Agent.py
@@ -12,3 +12,17 @@ class Q_Agent:
         self.num_bins = num_bins
         self.theta = np.random.uniform(-0.001, 0.001, (self.env.action_space.n,
                       num_tiles * num_bins * env.observation_space.shape[0]))
+
+    def choose_action(self, state):
+        if (random.random() < self.epsilon):
+            action = random.randint(0, self.env.action_space.n - 1)
+        else:
+            action = np.argmax(np.sum(self.theta * state, axis=1))
+        return action
+    
+    def update(self, state, action, reward, next_state, done):
+        if done:
+            target = reward
+        else:
+            target = reward + self.gamma * np.max(np.sum(self.theta * next_state, axis=1))
+        self.theta[action] += self.alpha * (target - np.sum(self.theta[action] * state)) * state
